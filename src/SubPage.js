caf7136d0c6e7ebfc4aa2f89f5582f6a35d71fcd
MallamTY <tnsosanya@student.oauife.edu.ng>
2024-01-18 19:51:35 +0100

    Second commit

---
+++ src/SubPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const SubPage = () => {
+    return (
+        <div>
+            <Link to="/">Go to main page !!!</Link>
+        </div>
+    )
+}
+
+
+export default SubPage;
